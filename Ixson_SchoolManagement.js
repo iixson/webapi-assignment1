e1f7f84e4db00458f39ae7c28542e9a62bb62170
Ixson <xiixsonz@gmail.com>
2024-11-13 23:13:03 +0800

    feat: Added Sample Data

--- Ixson_SchoolManagement.js
+++ Ixson_SchoolManagement.js
@@ -1,10 +1,41 @@
 module.exports = {
-    // Explain what function A does
-    functionA() {
-        return 1+2;
+  students: [
+    {
+      id: 1,
+      name: "Daniel Ong",
+      classid: 1,
     },
-    // Explain what function B does
-    functionB() {
-        console.log("Hello function B");
-    }
-}
+    {
+      id: 2,
+      name: "Josh Lee",
+      classid: 2,
+    },
+  ],
+
+  classes: [
+    { id: 1, name: "IM2250" },
+    { id: 2, name: "IM2251" },
+  ],
+
+  attendances: [
+    {
+      id: 1,
+      studentid: 1,
+      classid: 1,
+      date: "13/11/2024",
+      statusid: 1,
+    },
+    {
+      id: 2,
+      studentid: 2,
+      classid: 2,
+      date: "13/11/2024",
+      statusid: 1,
+    },
+  ],
+
+  status: [
+    { id: 1, name: "Present" },
+    { id: 2, name: "Absent" },
+  ],
+};
